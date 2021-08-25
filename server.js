52155c0e00c5de92e24daa2d773a4c5d419af9f3
Andy <andy.greenhaw@gmail.com>
2021-08-25 00:28:16 -0600

    Initial Commit: Set up all pages except for the React Shop App.

---
+++ server.js
@@ -0,0 +1,41 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Routes
+// =============================================================
+
+// Homepage Route
+app.get("/", function(req, res) {
+  // res.send("Welcome to the Star Wars Page!")
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+// Colorado Deals Page Route Route
+app.get("/colorado-deals", function(req, res) {
+  // res.send("Welcome to the Star Wars Page!")
+  res.sendFile(path.join(__dirname , "colorado-deals.html"));
+});
+
+// Michigan Deals Page Route Route
+app.get("/michigan-deals", function(req, res) {
+  // res.send("Welcome to the Star Wars Page!")
+  res.sendFile(path.join(__dirname , "michigan-deals.html"));
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
